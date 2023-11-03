565b2628004d72f73af70e5fb86598df0d416891
Zahid Hasan <zahidhasan140338@gmail.com>
2023-11-03 04:33:38 -0700

    server side has also been secured

--- index.js
+++ index.js
@@ -32,6 +32,32 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares
+const logger = async(req, res, next) =>{
+    console.log('called:', req.host, req.originalUrl);
+    next();
+}
+
+const verifyToken = async(req, res, next) =>{
+    const token = req.cookies?.token;
+    console.log('value of token in middleware', token);
+    if(!token){
+        return res.status(401).send({message: 'not authorized'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        // error
+        if(err){
+            console.log(err);
+            return res.status(401).send({message: 'unauthorized'});
+        }
+        // if token is valid then it would be decoded
+        console.log('value in the token', decoded);
+        req.user = decoded;
+        next();
+    })
+   
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -41,7 +67,7 @@ async function run() {
     const bookingCollection = client.db('carDoctor').collection('bookings');
 
     // this is the api to create token
-    app.post('/jwt', async(req, res) =>{
+    app.post('/jwt', logger, async(req, res) =>{
         const user = req.body;
         console.log(user);
         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
@@ -56,7 +82,7 @@ async function run() {
 
     
     // this is the api to get all the services data from the database to the server
-    app.get('/services', async(req, res) =>{
+    app.get('/services', logger, async(req, res) =>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);
@@ -75,9 +101,12 @@ async function run() {
     })
 
     // specific bookings paoar jonno
-    app.get('/bookings', async(req, res)=> {
+    app.get('/bookings', logger, verifyToken, async(req, res)=> {
         console.log(req.query.email);
-        console.log('I am the token', req.cookies.token);
+        console.log('user in the valid token', req.user);
+        if(req.query.email !== req.user.email){
+            return res.status(403).send({message: 'forbidden access'})
+        }
         let query = {};
         if(req.query?.email){
             query = { email: req.query.email }
